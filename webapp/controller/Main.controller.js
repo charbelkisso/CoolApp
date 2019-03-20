08a1884a037e7c2c92670809fdedf1b01ec7e62c
charbelkisso <charbelkisso@gmail.com>
2019-03-20 10:27:48 +0000

    attaching the controller

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,13 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
-
-	return Controller.extend("sap.ui.demo.CoolApp.controller.Main", {
-		onInit: function () {
-
-		}
-	});
-});
+sap.ui.define(
+	[
+		"sap/ui/core/mvc/Controller"
+	],
+	function (Controller) {
+		"use strict";
+		return Controller.extend("sap.ui.demo.CoolApp.controller.Main", {
+			onSayHello: function () {
+				alert("Hello from Controller");
+			}
+		});
+	}
+);
