2ddbea048aef37148e9e6b9d94d0a2ecd8fffe49
charbelkisso <charbelkisso@gmail.com>
2019-03-21 07:52:42 +0000

    manifest

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,30 +1,32 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"sap/ui/demo/CoolApp/model/models"
-], function (UIComponent, Device, models) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
 	"use strict";
-
+	
 	return UIComponent.extend("sap.ui.demo.CoolApp.Component", {
-
 		metadata: {
 			manifest: "json"
 		},
-
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
+		
 		init: function () {
-			// call the base component's init function
+			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
 
-			// enable routing
-			this.getRouter().initialize();
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
+			// set i18n model
+			var i18nModel = new ResourceModel({
+				bundleName: "sap.ui.demo.CoolApp.i18n.i18n"
+			});
+			this.setModel(i18nModel, "i18n");
 		}
 	});
 });
